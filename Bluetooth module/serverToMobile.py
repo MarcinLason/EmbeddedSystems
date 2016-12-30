bb09d067728a33466478d2d0b7c03cf4686eb401
karolkozak <karol2868@gmail.com>
2016-12-30 19:04:34 +0000

    serverToMobile.py improvements

--- Bluetooth module/serverToMobile.py
+++ Bluetooth module/serverToMobile.py
@@ -1,69 +1,65 @@
 import os
 import glob
-import time
-import RPi.GPIO as GPIO
 from bluetooth import *
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+# os.system('modprobe w1-gpio')
+# os.system('modprobe w1-therm')
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setup(17, GPIO.OUT)
 
-base_dir = '/sys/bus/w1/devices/'
+'''base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-	
-#while True:
-#	print(read_temp())	
-#	time.sleep(1)
+device_file = device_folder + '/w1_slave'''
 
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+# uuid = "00001105-0000-1000-8000-00805f9b34fb"
 
-advertise_service( server_sock, "CameraServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
+advertise_service(server_sock, "CameraServer",
+                   service_id=uuid,
+                   service_classes=[uuid, SERIAL_PORT_CLASS],
+                   profiles=[SERIAL_PORT_PROFILE],
+#                   protocols = [ OBEX_UUID ]
                     )
-while True:          
-	print "Waiting for connection on RFCOMM channel %d" % port
+while True:
+    print(("Waiting for connection on RFCOMM channel %d" % port))
 
-	client_sock, client_info = server_sock.accept()
-	print "Accepted connection from ", client_info
+    client_sock, client_info = server_sock.accept()
+    print(("Accepted connection from ", client_info))
 
-	try:
-	        data = client_sock.recv(1024)
-        	if len(data) == 0: break
-	        print "received [%s]" % data
+    try:
+        data = client_sock.recv(1024)
+        if len(data) == 0:
+            break
+        print(("received [%s]" % data))
 
-		if data == 'switch':
-			data = 'switched!'
-		elif data == 'capture':
-			data = 'capture!'
-		elif data == 'turnOff':
-			data = 'turn off!'
-		else:
-			data = 'WTF!' 
-	        client_sock.send(data)
-		print "sending [%s]" % data
+        if data == 'switch':
+            data = 'switched!'
+        elif data == 'capture':
+            data = 'capture!'
+        elif data == 'turnOff':
+            data = 'turn off!'
+        else:
+            data = 'WTF!'
+        client_sock.send(data)
+        print(("sending [%s]" % data))
 
-	except IOError:
-		pass
+    except IOError:
+        pass
 
-	except KeyboardInterrupt:
+    except KeyboardInterrupt:
 
-		print "disconnected"
+        print(("disconnected"))
 
-		client_sock.close()
-		server_sock.close()
-		print "all done"
+        client_sock.close()
+        server_sock.close()
+        print(("all done"))
 
-		break
+        break
