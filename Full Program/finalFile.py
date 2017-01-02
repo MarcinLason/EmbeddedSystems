6c1ff949daf8061b08652cf3c90a854bbea4c2fb
karolkozak <karol2868@gmail.com>
2017-01-02 23:05:34 +0100

    Copy/paste improvement

--- Full Program/finalFile.py
+++ Full Program/finalFile.py
@@ -7,74 +7,6 @@ import time
 import datetime as dt
 import wiringpi as GPIO
 
-initGPIO()
-# HELLO MESSAGES
-print("Photo mode\n")
-
-# BLUETOOTH SETUP
-server_sock = BluetoothSocket(RFCOMM)
-server_sock.bind(("", PORT_ANY))
-server_sock.listen(1)
-
-port = server_sock.getsockname()[1]
-
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-
-advertise_service(server_sock, "CameraServer",
-                  service_id=uuid,
-                  service_classes=[uuid, SERIAL_PORT_CLASS],
-                  profiles=[SERIAL_PORT_PROFILE],
-                  #                   protocols = [ OBEX_UUID ]
-                  )
-
-# MAIN PROGRAM LOOP
-MODE_FLAG = 0                            # [0 - photo; 1 - video; 2 - motion detector
-pir = MotionSensor(18)                   # motion detector input
-
-while True:
-    print("Waiting for connection on RFCOMM channel %d" % port)
-
-    client_sock, client_info = server_sock.accept()
-    print("Accepted connection from ", client_info)
-
-    try:
-        data = client_sock.recv(1024)
-        if len(data) == 0:
-            break
-        print("received [%s]" % data)
-
-        if data == 'switch':
-            data = 'switched!'
-            MODE_FLAG = changeMode(MODE_FLAG)
-        elif data == 'capture':
-            data = 'capture!'
-            if MODE_FLAG == 0:     # taking photo
-                capturePicture()
-            elif MODE_FLAG == 1:   # taking video
-                captureVideo()
-            elif MODE_FLAG == 2:   # taking video after motion detected
-                captureWithPIR()
-        elif data == 'turnOff':
-            data = 'turn off!'
-            cleanUp()
-        else:
-            data = 'WTF!'
-        client_sock.send(data)
-        print("sending [%s]" % data)
-
-    except IOError:
-        pass
-
-    except KeyboardInterrupt:
-
-        print("disconnected")
-
-        client_sock.close()
-        server_sock.close()
-        print("all done")
-
-        break
-
 
 # DEFS
 def initGPIO():
@@ -152,3 +84,72 @@ def cleanUp():
     # clean up bluetooth connection
     client_sock.close()
     server_sock.close()
+
+
+initGPIO()
+# HELLO MESSAGES
+print("Photo mode\n")
+
+# BLUETOOTH SETUP
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("", PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service(server_sock, "CameraServer",
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
+                  profiles=[SERIAL_PORT_PROFILE],
+                  #                   protocols = [ OBEX_UUID ]
+                  )
+
+# MAIN PROGRAM LOOP
+MODE_FLAG = 0                            # [0 - photo; 1 - video; 2 - motion detector
+pir = MotionSensor(18)                   # motion detector input
+
+while True:
+    print("Waiting for connection on RFCOMM channel %d" % port)
+
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
+
+    try:
+        data = client_sock.recv(1024)
+        if len(data) == 0:
+            break
+        print("received [%s]" % data)
+
+        if data == 'switch':
+            data = 'switched!'
+            MODE_FLAG = changeMode(MODE_FLAG)
+        elif data == 'capture':
+            data = 'capture!'
+            if MODE_FLAG == 0:     # taking photo
+                capturePicture()
+            elif MODE_FLAG == 1:   # taking video
+                captureVideo()
+            elif MODE_FLAG == 2:   # taking video after motion detected
+                captureWithPIR()
+        elif data == 'turnOff':
+            data = 'turn off!'
+            cleanUp()
+        else:
+            data = 'WTF!'
+        client_sock.send(data)
+        print("sending [%s]" % data)
+
+    except IOError:
+        pass
+
+    except KeyboardInterrupt:
+
+        print("disconnected")
+
+        client_sock.close()
+        server_sock.close()
+        print("all done")
+
+        break
