3d41ff36db20fdaea65de8228ba3a614b1c94f65
karolkozak <karol2868@gmail.com>
2017-01-02 22:53:21 +0100

    Clean code

--- Bluetooth module/serverToMobile.py
+++ Bluetooth module/serverToMobile.py
@@ -1,17 +1,4 @@
-import os
-import glob
 from bluetooth import *
-
-# os.system('modprobe w1-gpio')
-# os.system('modprobe w1-therm')
-
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(17, GPIO.OUT)
-
-'''base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'''
-
 
 server_sock = BluetoothSocket(RFCOMM)
 server_sock.bind(("", PORT_ANY))
@@ -20,25 +7,24 @@ server_sock.listen(1)
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-# uuid = "00001105-0000-1000-8000-00805f9b34fb"
 
 advertise_service(server_sock, "CameraServer",
-                   service_id=uuid,
-                   service_classes=[uuid, SERIAL_PORT_CLASS],
-                   profiles=[SERIAL_PORT_PROFILE],
-#                   protocols = [ OBEX_UUID ]
-                    )
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
+                  profiles=[SERIAL_PORT_PROFILE],
+                  #                   protocols = [ OBEX_UUID ]
+                  )
 while True:
-    print(("Waiting for connection on RFCOMM channel %d" % port))
+    print("Waiting for connection on RFCOMM channel %d" % port)
 
     client_sock, client_info = server_sock.accept()
-    print(("Accepted connection from ", client_info))
+    print("Accepted connection from ", client_info)
 
     try:
         data = client_sock.recv(1024)
         if len(data) == 0:
             break
-        print(("received [%s]" % data))
+        print("received [%s]" % data)
 
         if data == 'switch':
             data = 'switched!'
@@ -49,17 +35,17 @@ while True:
         else:
             data = 'WTF!'
         client_sock.send(data)
-        print(("sending [%s]" % data))
+        print("sending [%s]" % data)
 
     except IOError:
         pass
 
     except KeyboardInterrupt:
 
-        print(("disconnected"))
+        print("disconnected")
 
         client_sock.close()
         server_sock.close()
-        print(("all done"))
+        print("all done")
 
         break
